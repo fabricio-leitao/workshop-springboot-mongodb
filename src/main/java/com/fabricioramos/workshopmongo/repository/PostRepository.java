3bacf4f5d36e00cd559a01876de4caa26abed0da
Fabricio Leitao <fabricio-beco@hotmail.com>
2020-03-24 15:09:49 -0300

    Consulta simples com @Query

--- src/main/java/com/fabricioramos/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/fabricioramos/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.fabricioramos.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fabricioramos.workshopmongo.domain.Post;
@@ -10,5 +11,8 @@ import com.fabricioramos.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
